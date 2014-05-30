bb150743ac72b0d99a6e885d6b3112eee79ce38f
Ben Osheroff <ben@zendesk.com>
2014-05-30 11:03:51 -0700

    further on web frontend

--- web.rb
+++ web.rb
@@ -7,42 +7,48 @@ require 'lib/schema'
 require 'lib/binlog_dir'
 require 'lib/config'
 
-set :config, BinlogConfig.new
+class Web < Sinatra::Base
+  set :config, BinlogConfig.new
 
-before do
-  content_type :json
-end
-
-# capture the schema at a position.
-#
-# return both the schema and the current position of the binlog for future requests
-# required parameter: database
-get "/mark_binlog_top" do
-  db = params[:db]
-
-  if !db
-    status 422
-    body({err: "Please provide the 'db' parameter"}.to_json)
-    return
+  def require_params(*params)
+    missing = params.select { |p| !params[p] }
+    if missing.any?
+      status 422
+      body(err: "Please provide the following parameter(s): #{mising.join(',')}")
+      false
+    else
+      true
+    end
   end
 
-  schema = Schema.new(settings.config.mysql_connection, db)
-  s = schema.fetch
-  schema.save
-  status 200
-  body schema.binlog_info.merge(schema: s).to_json
-end
+  before do
+    content_type :json
+  end
 
-# begin playing the binlog from a specified position.
-# The position *must* be one returned from mark_binlog_top
-#
-# required parameters: account_id, database, start_file, start_pos
-# optional paramerers: end_file, end_pos
-#
-get "/binlog_events" do
+  # capture the schema at a position.
+  #
+  # return both the schema and the current position of the binlog for future requests
+  # required parameter: database
+  get "/mark_binlog_top" do
+    return unless require_params(:db)
+
+    schema = Schema.new(settings.config.mysql_connection, params[:db])
+    s = schema.fetch
+    schema.save
+    status 200
+    body schema.binlog_info.merge(schema: s).to_json
+  end
+
+  # begin playing the binlog from a specified position.
+  # The position *must* be one returned from mark_binlog_top
+  #
+  # required parameters: account_id, database, start_file, start_pos
+  # optional paramerers: end_file, end_pos
+  #
+  get "/binlog_events" do
 
 
-end
+  end
 
 
 
