db1b325140c36b9e5a67788952788a999c74a29d
Ben Osheroff <ben@zendesk.com>
2014-06-05 14:49:24 -0700

    some basic config setup

--- lib/config.rb
+++ lib/config.rb
@@ -1,10 +1,19 @@
 require 'mysql2'
 class BinlogConfig
+  BINLOG_SERVICE_CONFIG = File.join(File.dirname(__FILE__) + "/../config/exodus_binlog_service.yml")
+
+  def config
+    @config ||= YAML.load_file(BINLOG_SERVICE_CONFIG)
+  end
   def binlog_dir
-    "/opt/local/var/db/mysql5"
+    config['binlog_dir']
   end
 
   def mysql_connection
-    @mysql_connection ||= Mysql2::Client.new(username: 'root')
+    @mysql_connection ||= Mysql2::Client.new(username: config['mysql']['username'], password: config['mysql']['password'])
+  end
+
+  def api_port
+    config['port']
   end
 end
