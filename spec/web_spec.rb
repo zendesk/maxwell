d9a4cbc2cf77c96de43441e7e5940d80411faf8d
Ben Osheroff <ben@zendesk.com>
2014-06-25 12:39:24 -0700

    Get travis running
    Lots of mysql_isolated_server stuff, also have the web tests use the
    isolated server instance.

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -7,13 +7,28 @@ describe "the api" do
   include Rack::Test::Methods
 
   def app
-    Web
+    h = {
+         'binlog_dir' => $mysql_binlog_dir,
+         'mysql' => {'username' => 'root', 'password' => '', 'host' => '127.0.0.1', 'port' => $mysql_master.port}
+        }
+
+    config = BinlogConfig.new
+    config.config = h
+    Web.set(:config, config)
+    Web.new
   end
 
   describe "/mark_binlog_top" do
-    it "does some stuff" do
-      get "/mark_binlog_top?db=dev1"
+    before do
+      get "/mark_binlog_top?db=shard_1"
+    end
+
+    it "returns ok" do
       expect(last_response).to be_ok
+    end
+
+    it "returns json" do
+
     end
   end
 end
