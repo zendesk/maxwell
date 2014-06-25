ba76f582d3f31b68b1121d39dc97a2ee3c3d1fc9
Ben Osheroff <ben@zendesk.com>
2014-06-25 10:05:56 -0700

    remove row filter, cleanup whitespace

--- java/src/com/zendesk/exodus/ExodusParser.java
+++ java/src/com/zendesk/exodus/ExodusParser.java
@@ -12,19 +12,17 @@ import com.google.code.or.common.util.MySQLConstants;
 import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class ExodusParser {
-	
+
 	String filePath, fileName;
 	private long startPosition;
-	
+
 	public String getFilePath() {
 		return filePath;
 	}
@@ -36,27 +34,21 @@ public class ExodusParser {
 	public void setStartPosition(long pos) {
 		this.startPosition = pos;
 	}
-	
-	private LinkedBlockingQueue<BinlogEventV4> queue =  new LinkedBlockingQueue<BinlogEventV4>(100);
+
+	private final LinkedBlockingQueue<BinlogEventV4> queue =  new LinkedBlockingQueue<BinlogEventV4>(100);
 
 	protected FileBasedBinlogParser parser;
 	protected ExodusBinlogEventListener binlogEventListener;
-	private Map<Integer, ExodusRowFilter> rowFilters = new HashMap<Integer, ExodusRowFilter>();
-	
 
 	public ExodusParser(String filePath, String fileName) throws Exception {
 		this.filePath = filePath;
 		this.fileName = fileName;
 		this.startPosition = 4;
-		
+
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		this.binlogEventListener = new ExodusBinlogEventListener(queue);
 	}
 
-	public void addRowFilter(ExodusRowFilter f) {
-		this.rowFilters.put(f.getTableId(), f);
-	}	
-	
 	public BinlogEventV4 getEvent() throws Exception {
 		BinlogEventV4 event;
 
@@ -67,12 +59,12 @@ public class ExodusParser {
 
 		while (true) {
 			event = queue.poll(100, TimeUnit.MILLISECONDS);
-			if (event != null) { 
+			if (event != null) {
 				if ( event instanceof RotateEvent ) {
 					RotateEvent r = (RotateEvent) event;
-					// we throw out the old parser and boot a new one. 
+					// we throw out the old parser and boot a new one.
 					initParser(r.getBinlogFileName().toString(), r.getBinlogPosition());
-					this.parser.start();   
+					this.parser.start();
 				} else {
 					return event;
 				}
@@ -82,16 +74,16 @@ public class ExodusParser {
 					   Likely that we've simply hit the end, but another possibility
 					   is that the server crashed and never inserted the "rotate" event in
 				       the logs.  Let's test for that. */
-					
+
 					String nextFile = findNextBinlogFile(this.fileName);
 					if (nextFile == null)
-						return null; 
+						return null;
 					else {
 						initParser(nextFile, 4);
 						this.parser.start();
 					}
 				}
-			}		
+			}
 		}
 	}
 
@@ -100,7 +92,7 @@ public class ExodusParser {
 		Matcher m = p.matcher(fileName);
 		if ( !m.matches() )
 			return null;
-		
+
 		Integer nextInt = Integer.valueOf(m.group(2)) + 1;
 		String testFile = m.group(1) + "." + String.format("%06d", nextInt);
 		File f = new File(this.filePath + "/" + testFile);
@@ -134,26 +126,26 @@ public class ExodusParser {
 
 	private void initParser(String fileName, long position) throws Exception {
 		FileBasedBinlogParser bp = getDefaultBinlogParser();
-		
+
 		bp.setStartPosition(position);
 		bp.setBinlogFileName(fileName);
 		bp.setBinlogFilePath(filePath);
-		
+
 		bp.setEventListener(this.binlogEventListener);
-				
-		bp.setEventFilter(new BinlogEventFilter() { 
-			public boolean accepts(BinlogEventV4Header header, BinlogParserContext context) { 
+
+		bp.setEventFilter(new BinlogEventFilter() {
+			public boolean accepts(BinlogEventV4Header header, BinlogParserContext context) {
 				int eventType = header.getEventType();
-				return eventType == MySQLConstants.WRITE_ROWS_EVENT || 
+				return eventType == MySQLConstants.WRITE_ROWS_EVENT ||
 						eventType == MySQLConstants.WRITE_ROWS_EVENT_V2 ||
 						eventType == MySQLConstants.UPDATE_ROWS_EVENT ||
 						eventType == MySQLConstants.UPDATE_ROWS_EVENT_V2 ||
 						eventType == MySQLConstants.DELETE_ROWS_EVENT ||
 						eventType == MySQLConstants.DELETE_ROWS_EVENT_V2 ||
-						eventType == MySQLConstants.TABLE_MAP_EVENT || 
+						eventType == MySQLConstants.TABLE_MAP_EVENT ||
 						eventType == MySQLConstants.ROTATE_EVENT;
 			}
-		}); 
+		});
 		this.fileName = fileName;
 		this.startPosition = position;
 		this.parser = bp;
@@ -164,36 +156,31 @@ public class ExodusParser {
 		BinlogEventV4 e;
 		ExodusParser p = new ExodusParser("/opt/local/var/db/mysql5", "master.000007");
 		Date start = new Date();
-		ExodusRowFilter f = new ExodusRowFilter(1, 3, 1);
-		p.addRowFilter(f);
+
 		while ((e = p.getEvent()) != null) {
 			if ( e instanceof AbstractRowEvent ) {
 				HashMap<String, Object> filter = new HashMap<String, Object>();
 				String columns[] = new String[2];
-				
+
 				columns[0] = "foo";
 				columns[1] = "bar";
-				
+
 				String encodings[] = new String[100];
 				for (int i = 0 ; i < 100; i++) {
 					encodings[i] = "utf8";
 				}
-				
+
 				String s = ExodusAbstractRowsEvent.buildEvent((AbstractRowEvent) e, "nothing", columns, encodings, 1).toSql(filter);
 				System.out.println(s);
 			}
-			
+
 			count++;
 			if ( count % 1000 == 0 ) {
 				Date d = new Date();
 				System.out.println("did " + Integer.toString(count) + " at " + (d.getTime() - start.getTime()) );
 			}
-			
-			//System.out.println(e.toString());
 		}
 	}
-
-
 }
 
 
