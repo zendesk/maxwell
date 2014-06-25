df1a2d410d58e2bfded129164685d231c7090aa1
Ben Osheroff <ben@zendesk.com>
2014-06-25 12:39:24 -0700

    Get travis running
    Lots of mysql_isolated_server stuff, also have the web tests use the
    isolated server instance.

--- lib/config.rb
+++ lib/config.rb
@@ -2,16 +2,31 @@ require 'lib/jdbc_connection'
 
 class BinlogConfig
   BINLOG_SERVICE_CONFIG = File.join(File.dirname(__FILE__) + "/../config/exodus_binlog_service.yml")
+  def initialize(config_file = nil)
+    @config_file = (config_file || BINLOG_SERVICE_CONFIG)
+  end
+
+  def load_config!
+    YAML.load_file(@config_file)
+  end
 
   def config
-    @config ||= YAML.load_file(BINLOG_SERVICE_CONFIG)
+    @config ||= load_config!
   end
+
+  def config=(config)
+    @config = config
+  end
+
   def binlog_dir
     config['binlog_dir']
   end
 
   def mysql_connection
-    @mysql_connection ||= WrappedJDBCConnection.new(username: config['mysql']['username'], password: config['mysql']['password'])
+    @mysql_connection ||= WrappedJDBCConnection.new(username: config['mysql']['username'],
+                                                    password: config['mysql']['password'],
+                                                    host: config['mysql']['host'],
+                                                    port: config['mysql']['port'])
   end
 
   def api_port
