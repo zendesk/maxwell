48df8232c97dcb1da505f70c7e944febf1eadbc7
Ben Osheroff <ben@zendesk.com>
2015-02-06 14:31:21 -0800

    extract ColumnPosition to a separate file

--- java/src/com/zendesk/exodus/schema/ddl/ExodusMysqlParserListener.java
+++ java/src/com/zendesk/exodus/schema/ddl/ExodusMysqlParserListener.java
@@ -10,36 +10,6 @@ import com.zendesk.exodus.schema.columndef.ColumnDef;
 import com.zendesk.exodus.schema.ddl.mysqlParser.Table_nameContext;
 import com.zendesk.exodus.schema.ddl.mysqlParser.*;
 
-class ColumnPosition {
-	enum Position { FIRST, AFTER, DEFAULT };
-
-	public Position position;
-	public String afterColumn;
-
-	public ColumnPosition() {
-		position = Position.DEFAULT;
-	}
-
-	public int index(Table t, Integer defaultIndex) throws SchemaSyncError {
-		switch(position) {
-		case FIRST:
-			return 0;
-		case DEFAULT:
-			if ( defaultIndex != null )
-				return defaultIndex;
-			else
-				return t.getColumnList().size();
-
-		case AFTER:
-			int afterIdx = t.findColumnIndex(afterColumn);
-			if ( afterIdx == -1 )
-				throw new SchemaSyncError("Could not find column " + afterColumn + " (needed in AFTER statement)");
-			return afterIdx + 1;
-		}
-		return -1;
-	}
-}
-
 class ExodusSQLSyntaxRrror extends RuntimeException {
 	private static final long serialVersionUID = 140545518818187219L;
 
