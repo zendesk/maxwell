4752a1d9a07512d149c0ce545fcbf47b9f4097fc
Ben Osheroff <ben@zendesk.com>
2015-09-17 13:35:41 -0700

    don't allow maxwell to start when binlog_row_image == minimal

--- src/main/java/com/zendesk/maxwell/MaxwellMysqlStatus.java
+++ src/main/java/com/zendesk/maxwell/MaxwellMysqlStatus.java
@@ -15,13 +15,17 @@ public class MaxwellMysqlStatus {
 		return "SHOW VARIABLES LIKE '" + variableName + "'";
 	}
 
-	private String getVariableState(String variableName) throws SQLException, MaxwellCompatibilityError {
+	private String getVariableState(String variableName, boolean throwOnMissing) throws SQLException, MaxwellCompatibilityError {
 		ResultSet rs;
 
 		rs = connection.createStatement().executeQuery(sqlStatement(variableName));
 		String status;
 		if(!rs.next()) {
-			throw new MaxwellCompatibilityError("Could not check state for Mysql variable: "+variableName);
+			if ( throwOnMissing ) {
+				throw new MaxwellCompatibilityError("Could not check state for Mysql variable: " + variableName);
+			} else {
+				return null;
+			}
 		}
 
 		status = rs.getString("Value");
@@ -29,21 +33,30 @@ public class MaxwellMysqlStatus {
 	}
 
 	private void ensureNotReadOnly() throws SQLException, MaxwellCompatibilityError {
-		if (!getVariableState("read_only").equals("OFF")) {
+		if (!getVariableState("read_only", true).equals("OFF")) {
 			throw new MaxwellCompatibilityError("read_only must be OFF.");
 		}
 	}
 
 	private void ensureReplicationOn() throws SQLException, MaxwellCompatibilityError {
-		if (!getVariableState("log_bin").equals("ON")) {
+		if (!getVariableState("log_bin", true).equals("ON")) {
 			throw new MaxwellCompatibilityError("log_bin status must be ON.");
 		}
 	}
 
 	private void ensureReplicationFormatROW() throws SQLException, MaxwellCompatibilityError {
-		if (!getVariableState("binlog_format").equals("ROW")) {
+		if (!getVariableState("binlog_format", true).equals("ROW")) {
 			throw new MaxwellCompatibilityError("binlog_format must be ROW.");
 		}
+	}
+
+	private void ensureRowImageFormat() throws SQLException, MaxwellCompatibilityError {
+		String rowImageFormat = getVariableState("binlog_row_image", false);
+		if ( rowImageFormat == null ) // only present in mysql 5.6+
+			return;
+
+		if ( !rowImageFormat.equals("FULL"))
+			throw new MaxwellCompatibilityError("binlog_row_image must be FULL.");
 	}
 
 	public static void ensureMysqlState(Connection c) throws SQLException, MaxwellCompatibilityError {
@@ -51,5 +64,6 @@ public class MaxwellMysqlStatus {
 		m.ensureNotReadOnly();
 		m.ensureReplicationOn();
 		m.ensureReplicationFormatROW();
+		m.ensureRowImageFormat();
 	}
 }
