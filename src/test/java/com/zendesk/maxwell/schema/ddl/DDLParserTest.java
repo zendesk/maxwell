eb61c273489926dfd36af81f3664eb0de64f884c
Ben Osheroff <ben@zendesk.com>
2015-12-10 21:03:21 -0800

    probably parse CREATE TABLE ( ... ) ENGINE = MEMORY;
    antlr's tokenizer picks up the bareword MEMORY as the token MEMORY and
    thus it won't match IDENT or STRING_LITERAL

--- src/test/java/com/zendesk/maxwell/schema/ddl/DDLParserTest.java
+++ src/test/java/com/zendesk/maxwell/schema/ddl/DDLParserTest.java
@@ -191,7 +191,8 @@ public class DDLParserTest {
 			"alter table t add column `foo` int,  ALGORITHM=copy",
 			"alter table t add column `foo` int, algorithm copy",
 			"alter table t add column `foo` int, algorithm copy, lock shared",
-			"alter table t add column `foo` int, algorithm copy, lock=exclusive"
+			"alter table t add column `foo` int, algorithm copy, lock=exclusive",
+			"create table t (id int) engine=memory"
 		};
 
 		for ( String s : testSQL ) {
