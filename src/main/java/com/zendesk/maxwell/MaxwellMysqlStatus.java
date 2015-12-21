57d3ad86826b3c690b35d123005b446a1c0f1962
Kristian Kaufmann <kristianwkaufmann@gmail.com>
2015-12-21 14:22:56 -0500

    removing commented out line

--- src/main/java/com/zendesk/maxwell/MaxwellMysqlStatus.java
+++ src/main/java/com/zendesk/maxwell/MaxwellMysqlStatus.java
@@ -52,7 +52,6 @@ public class MaxwellMysqlStatus {
 	public static void ensureMysqlState(Connection c) throws SQLException, MaxwellCompatibilityError {
 		MaxwellMysqlStatus m = new MaxwellMysqlStatus(c);
 
-		//m.ensureVariableState("read_only", "OFF");
 		m.ensureVariableState("log_bin", "ON");
 		m.ensureVariableState("binlog_format", "ROW");
 		m.ensureRowImageFormat();
