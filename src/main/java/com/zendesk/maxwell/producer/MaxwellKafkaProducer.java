9c1b39fec1997836cc13106fba63d92a3e4fc8fc
Ben Osheroff <ben@zendesk.com>
2016-03-17 15:38:39 -0700

    move towards changing the kafka key format
    The old keyformat was nicely descriptive, but unfortunately it was quite
    hard to reproduce in any library that didn't do direct JSON
    serialization like maxwell does; so re-processing the stream in
    KStreams sucked.

--- src/main/java/com/zendesk/maxwell/producer/MaxwellKafkaProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/MaxwellKafkaProducer.java
@@ -9,6 +9,7 @@ import com.zendesk.maxwell.MaxwellAbstractRowsEvent;
 import com.zendesk.maxwell.MaxwellContext;
 
 import com.zendesk.maxwell.RowMap;
+import com.zendesk.maxwell.RowMap.KeyFormat;
 import com.zendesk.maxwell.producer.partitioners.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -64,6 +65,7 @@ public class MaxwellKafkaProducer extends AbstractProducer {
 	private String topic;
 	private final int numPartitions;
 	private final MaxwellKafkaPartitioner partitioner;
+	private final KeyFormat keyFormat;
 
 	public MaxwellKafkaProducer(MaxwellContext context, Properties kafkaProperties, String kafkaTopic) {
 		super(context);
@@ -80,14 +82,20 @@ public class MaxwellKafkaProducer extends AbstractProducer {
 		String hash = context.getConfig().kafkaPartitionHash;
 		String partitionKey = context.getConfig().kafkaPartitionKey;
 		this.partitioner = new MaxwellKafkaPartitioner(hash, partitionKey);
+
+		if ( context.getConfig().kafkaKeyFormat.equals("hash") )
+			keyFormat = KeyFormat.HASH;
+		else
+			keyFormat = KeyFormat.ARRAY;
 	}
 
 	@Override
 	public void push(RowMap r) throws Exception {
-		String key = r.pkToJson();
+		String key = r.pkToJson(keyFormat);
 		String value = r.toJSON();
+
 		ProducerRecord<String, String> record =
-				new ProducerRecord<>(topic, this.partitioner.kafkaPartition(r, this.numPartitions), r.pkToJson(), r.toJSON());
+				new ProducerRecord<>(topic, this.partitioner.kafkaPartition(r, this.numPartitions), key, value);
 
 		kafka.send(record, new KafkaCallback(r, this.context, key, value));
 	}
