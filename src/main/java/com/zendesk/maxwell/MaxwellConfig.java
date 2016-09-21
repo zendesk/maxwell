1d5637f6d15dd3f5b9730a4a6ac6dfd3d5839ecf
Ben Osheroff <ben@zendesk.com>
2016-09-21 12:36:36 -0700

    change negative-facing argument to positive facing.

--- src/main/java/com/zendesk/maxwell/MaxwellConfig.java
+++ src/main/java/com/zendesk/maxwell/MaxwellConfig.java
@@ -93,7 +93,7 @@ public class MaxwellConfig extends AbstractConfig {
 
 		parser.accepts( "output_binlog_position", "produced records include binlog position; [true|false]. default: false" ).withOptionalArg();
 		parser.accepts( "output_commit_info", "produced records include commit and xid; [true|false]. default: true" ).withOptionalArg();
-		parser.accepts( "output_omit_null", "produced records exclude fields with NULL values [true|false]. default: false" ).withOptionalArg();
+		parser.accepts( "output_nulls", "produced records include fields with NULL values [true|false]. default: true" ).withOptionalArg();
 
 		parser.accepts( "__separator_5" );
 
@@ -276,7 +276,7 @@ public class MaxwellConfig extends AbstractConfig {
 		this.outputConfig = new MaxwellOutputConfig();
 		outputConfig.includesBinlogPosition = fetchBooleanOption("output_binlog_position", options, properties, false);
 		outputConfig.includesCommitInfo = fetchBooleanOption("output_commit_info", options, properties, true);
-		outputConfig.omitNull = fetchBooleanOption("output_omit_null", options, properties, true);
+		outputConfig.includesNulls = fetchBooleanOption("output_nulls", options, properties, true);
 	}
 
 	private Properties parseFile(String filename, Boolean abortOnMissing) {
