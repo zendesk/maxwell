d586fab377058cb8cb58a3f43f912792989f9227
Ben Osheroff <ben@zendesk.com>
2016-12-19 10:15:31 -0800

    update javadocs for AbstractReplicator

--- src/main/java/com/zendesk/maxwell/replication/AbstractReplicator.java
+++ src/main/java/com/zendesk/maxwell/replication/AbstractReplicator.java
@@ -41,9 +41,10 @@ public abstract class AbstractReplicator extends RunLoopProcess implements Repli
 	 * Possibly convert a RowMap object into a HeartbeatRowMap
 	 *
 	 * Process a rowmap that represents a write to `maxwell`.`heartbeats`.
-	 * If it's a write for a different client_id, we return just the RowMap,
-	 * which will be ignored.  Otherwise, we transform it into a
-	 * HeartbeatRowMap and set lastHeartbeatRead.
+	 * If it's a write for a different client_id, we return the input (which
+	 * will signify to the rest of the chain to ignore it).  Otherwise, we
+	 * transform it into a HeartbeatRowMap (which will not be output, but will
+	 * advance the binlog position) and set `this.lastHeartbeatRead`
 	 *
 	 * @return either a RowMap or a HeartbeatRowMap
 	 */
@@ -57,6 +58,15 @@ public abstract class AbstractReplicator extends RunLoopProcess implements Repli
 		return HeartbeatRowMap.valueOf(row.getDatabase(), row.getPosition(), this.lastHeartbeatRead);
 	}
 
+	/**
+	 * Parse a DDL statement and output the results to the producer
+	 *
+	 * @param dbName The database "context" under which the SQL is to be processed.  think "use db; alter table foo ..."
+	 * @param sql The DDL SQL to be processed
+	 * @param schemaStore A SchemaStore object to which we delegate the parsing of the sql
+	 * @param position The position that the SQL happened at
+	 * @param timestmap The timestamp of the SQL binlog event
+	 */
 	protected void processQueryEvent(String dbName, String sql, SchemaStore schemaStore, BinlogPosition position, Long timestamp) throws Exception {
 		List<ResolvedSchemaChange> changes =  schemaStore.processSQL(sql, dbName, position);
 		for ( ResolvedSchemaChange change : changes ) {
@@ -70,13 +80,23 @@ public abstract class AbstractReplicator extends RunLoopProcess implements Repli
 			this.producer.writePosition(position);
 	}
 
+	/**
+	 * Should we output an event for the given database and table?
+	 *
+	 * Here we check against a whitelist/blacklist/filter.  The whitelist
+	 * passes updates to `maxwell.bootstrap` through (those are control
+	 * mechanisms for bootstrap), the blacklist gets rid of the
+	 * `ha_health_check` table which shows up oddly in Amazon RDS.
+	 *
+	 * @param database The database of the DML
+	 * @param table The table of the DML
+	 * @param filter A table-filter, or null
+	 * @return Whether we should write the event to the producer
+	 */
 	protected boolean shouldOutputEvent(String database, String table, MaxwellFilter filter) {
-		/* always pass bootstrap rows through */
 		Boolean isSystemWhitelisted = database.equals(this.maxwellSchemaDatabaseName)
 			&& table.equals("bootstrap");
 
-		/* there's an odd RDS thing, I guess, where ha_health_check doesn't
-		 * show up in INFORMATION_SCHEMA but it's replicated nonetheless. */
 		Boolean isSystemBlacklisted = database.equals("mysql") && table.equals("ha_health_check");
 		if ( isSystemBlacklisted )
 			return false;
@@ -90,13 +110,18 @@ public abstract class AbstractReplicator extends RunLoopProcess implements Repli
 	 * Get the last heartbeat that the replicator has processed.
 	 *
 	 * We pass along the value of the heartbeat to the producer inside the row map.
-	 * @return the millisecond value ot fhte last heartbeat
+	 * @return the millisecond value ot the last heartbeat read
 	 */
 
 	public Long getLastHeartbeatRead() {
 		return lastHeartbeatRead;
 	}
 
+	/**
+	 * get a single row from the replicator and pass it to the producer or bootstrapper.
+	 *
+	 * This is the top-level function in the run-loop.
+	 */
 	public void work() throws Exception {
 		RowMap row = getRow();
 
@@ -117,6 +142,13 @@ public abstract class AbstractReplicator extends RunLoopProcess implements Repli
 			bootstrapper.work(row, producer, this);
 	}
 
+	/**
+	 * Is this RowMap an update to one of maxwell's own tables?
+	 *
+	 * If so we will often suppress the output.
+	 * @param row The RowMap in question
+	 * @return whether the update is something maxwell itself generated
+	 */
 	protected boolean isMaxwellRow(RowMap row) {
 		return row.getDatabase().equals(this.maxwellSchemaDatabaseName);
 	}
