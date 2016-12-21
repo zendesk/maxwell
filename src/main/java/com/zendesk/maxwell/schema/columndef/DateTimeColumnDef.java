4d8294bce0536f5571982ad744d9cc5aaca88612
Ben Osheroff <ben@zendesk.com>
2016-12-21 11:24:43 -0800

    initial work to port to shyko's binlog connector

--- src/main/java/com/zendesk/maxwell/schema/columndef/DateTimeColumnDef.java
+++ src/main/java/com/zendesk/maxwell/schema/columndef/DateTimeColumnDef.java
@@ -3,15 +3,25 @@ package com.zendesk.maxwell.schema.columndef;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import com.google.code.or.common.util.MySQLConstants;
+import com.zendesk.maxwell.MaxwellConfig;
 
 public class DateTimeColumnDef extends ColumnDefWithLength {
 	public DateTimeColumnDef(String name, String type, int pos, Long columnLength) {
 		super(name, type, pos, columnLength);
 	}
 
-	private static SimpleDateFormat dateTimeFormatter;
+	private static SimpleDateFormat dateTimeFormatter, dateTimeFormatterUTC;
+
+	private static SimpleDateFormat getDateTimeFormatterUTC() {
+		if ( dateTimeFormatterUTC == null ) {
+			dateTimeFormatterUTC = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			dateTimeFormatterUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+		}
+		return dateTimeFormatterUTC;
+	}
 
 	private static SimpleDateFormat getDateTimeFormatter() {
 		if ( dateTimeFormatter == null ) {
@@ -34,10 +44,20 @@ public class DateTimeColumnDef extends ColumnDefWithLength {
 	protected String formatValue(Object value) {
 		/* protect against multithreaded access of static dateTimeFormatter */
 		synchronized ( DateTimeColumnDef.class ) {
-			if ( value instanceof Long && getType().equals("datetime") ) {
+			if ( MaxwellConfig.ShykoMode && value instanceof Long ) {
+				/* BinlogConnector gives us long values; milliseconds since epoch. */
+				Long longVal = (Long) value;
+				Timestamp ts = new Timestamp(longVal);
+				String dateString = getDateTimeFormatterUTC().format(ts);
+
+				return objectWithPrecisionToString(dateString, ts, columnLength);
+
+			} else if ( value instanceof Long && getType().equals("datetime") ) {
+				/* used?  unused? */
 				return formatLong(( Long ) value);
 
 			} else if ( value instanceof Timestamp ) {
+				/* the bootstrapper should end up here.  so should open-replicator. */
 				Timestamp ts = (Timestamp) value;
 				String datetimeAsStr = getDateTimeFormatter().format(ts);
 
