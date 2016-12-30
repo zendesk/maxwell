57a9b7d24c9b4b523ee0bb278eed3d73bf34dcd0
Hitesh <hitesh.mehta@paytm.com>
2016-12-31 01:01:10 +0530

    Fixed issue of saving binlog position in event of maxwell restart

--- src/main/java/com/zendesk/maxwell/producer/RabbitmqProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/RabbitmqProducer.java
@@ -4,15 +4,21 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.zendesk.maxwell.MaxwellContext;
+import com.zendesk.maxwell.replication.BinlogPosition;
 import com.zendesk.maxwell.row.RowMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Created by hitesh on 30/12/16.
  */
 public class RabbitmqProducer extends AbstractProducer {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqProducer.class);
     private static String exchangeName;
     private final InflightMessageList inflightMessages;
     private Connection connection;
@@ -42,7 +48,26 @@ public class RabbitmqProducer extends AbstractProducer {
             return;
         }
         String routingKey = r.getDatabase() + "." + r.getTable();
+
         channel.basicPublish(exchangeName, routingKey, null, value.getBytes());
-        System.out.println(" [x] Sent '" + value + "'");
+        if ( r.isTXCommit() ) {
+            inflightMessages.addMessage(r.getPosition());
+            BinlogPosition newPosition = inflightMessages.completeMessage(r.getPosition());
+
+            if ( newPosition != null ) {
+                context.setPosition(newPosition);
+            }
+        }
+        if ( LOGGER.isDebugEnabled()) {
+            LOGGER.debug("->  routing key:" + routingKey + ", partition:" + value);
+        }
+    }
+
+    @Override
+    public void writePosition(BinlogPosition p) throws SQLException {
+        // ensure that we don't prematurely advance the binlog pointer.
+        inflightMessages.addMessage(p);
+        inflightMessages.completeMessage(p);
     }
 }
+
