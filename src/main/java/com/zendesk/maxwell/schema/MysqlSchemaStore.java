1410e8a52bf0776a66535860be8708dbe82d0a3e
Jeroen Minnaert <jeroenminnaert@gmail.com>
2017-02-09 18:02:43 -0800

    Added better validity checking

--- src/main/java/com/zendesk/maxwell/schema/MysqlSchemaStore.java
+++ src/main/java/com/zendesk/maxwell/schema/MysqlSchemaStore.java
@@ -65,14 +65,14 @@ public class MysqlSchemaStore extends AbstractSchemaStore implements SchemaStore
 			if ( savedSchema == null ) {
 				Schema capturedSchema = captureSchema();
 				savedSchema = new MysqlSavedSchema(serverID, caseSensitivity, capturedSchema, initialPosition);
-				if ( !readOnly )
-					if(conn.isClosed()){
+				if (!readOnly)
+					if (conn.isValid(30)) {
+						savedSchema.save(conn);
+					} else {
 						// The capture time might be long and the conn connection might be closed already. Consulting the pool
 						// again for a new connection
 						Connection newConn = maxwellConnectionPool.getConnection();
 						savedSchema.save(newConn);
-					} else {
-						savedSchema.save(conn);
 					}
 			}
 
