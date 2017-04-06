c487d002055d8e7a61aa420363b0adcc7fbae2b7
Thomas Ploch <thomas.ploch@flixbus.com>
2017-04-06 10:28:22 +0200

    FX. Whitespace -> Tabs in `RabbitProducer.java`

--- src/main/java/com/zendesk/maxwell/producer/RabbitmqProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/RabbitmqProducer.java
@@ -11,47 +11,43 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-/**
- * Created by hitesh on 30/12/16.
- */
 public class RabbitmqProducer extends AbstractProducer {
 
-    static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqProducer.class);
-    private static String exchangeName;
-    private Connection connection;
-    private Channel channel;
-    public RabbitmqProducer(MaxwellContext context) {
-        super(context);
-        this.exchangeName = context.getConfig().rabbitmqExchange;
+	static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqProducer.class);
+	private static String exchangeName;
+	private Connection connection;
+	private Channel channel;
+	public RabbitmqProducer(MaxwellContext context) {
+		super(context);
+		this.exchangeName = context.getConfig().rabbitmqExchange;
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(context.getConfig().rabbitmqHost);
-        try {
-            this.connection = factory.newConnection();
-            this.channel = connection.createChannel();
-            this.channel.exchangeDeclare(exchangeName, context.getConfig().rabbitmqExchangeType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
-    }
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(context.getConfig().rabbitmqHost);
+		try {
+			this.connection = factory.newConnection();
+			this.channel = connection.createChannel();
+			this.channel.exchangeDeclare(exchangeName, context.getConfig().rabbitmqExchangeType);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void push(RowMap r) throws Exception {
-        String value = r.toJSON(outputConfig);
-        if (value == null) {
-            return;
-        }
-        String routingKey = r.getDatabase() + "." + r.getTable();
+	@Override
+	public void push(RowMap r) throws Exception {
+		String value = r.toJSON(outputConfig);
+		if (value == null) {
+			return;
+		}
+		String routingKey = r.getDatabase() + "." + r.getTable();
 
-        channel.basicPublish(exchangeName, routingKey, null, value.getBytes());
-        if ( r.isTXCommit() ) {
-            context.setPosition(r.getPosition());
-        }
-        if ( LOGGER.isDebugEnabled()) {
-            LOGGER.debug("->  routing key:" + routingKey + ", partition:" + value);
-        }
-    }
+		channel.basicPublish(exchangeName, routingKey, null, value.getBytes());
+		if ( r.isTXCommit() ) {
+			context.setPosition(r.getPosition());
+		}
+		if ( LOGGER.isDebugEnabled()) {
+			LOGGER.debug("->  routing key:" + routingKey + ", partition:" + value);
+		}
+	}
 }
-
