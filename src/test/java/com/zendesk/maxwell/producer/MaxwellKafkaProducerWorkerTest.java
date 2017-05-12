0e1cfe39cfc0d9f9a94e4d5e3248370176191254
Likun Liu <lliu@zendesk.com>
2017-05-12 14:59:55 +1000

    test MaxwellKafkaProducerWorker with null topic

---
+++ src/test/java/com/zendesk/maxwell/producer/MaxwellKafkaProducerWorkerTest.java
@@ -0,0 +1,25 @@
+package com.zendesk.maxwell.producer;
+
+import com.zendesk.maxwell.MaxwellConfig;
+import com.zendesk.maxwell.MaxwellContext;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MaxwellKafkaProducerWorkerTest {
+
+	@Test
+	public void constructNewWorkerWithNullTopic() {
+		MaxwellContext context = mock(MaxwellContext.class);
+		MaxwellConfig config = new MaxwellConfig();
+		when(context.getConfig()).thenReturn(config);
+		Properties kafkaProperties = new Properties();
+		kafkaProperties.put("bootstrap.servers", "localhost:9092");
+		String kafkaTopic = null;
+		//shouldn't throw NPE
+		new MaxwellKafkaProducerWorker(context, kafkaProperties, kafkaTopic, null);
+	}
+}
