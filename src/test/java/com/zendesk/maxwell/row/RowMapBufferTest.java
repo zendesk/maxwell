f371843f60559037b4f74fca2d23e9d507d6e42f
Tim Cuthbertson <tim@gfxmonk.net>
2017-06-02 14:30:40 +1000

    fix failing tests due to RowMap timestamp change

--- src/test/java/com/zendesk/maxwell/row/RowMapBufferTest.java
+++ src/test/java/com/zendesk/maxwell/row/RowMapBufferTest.java
@@ -16,9 +16,9 @@ public class RowMapBufferTest extends TestWithNameLogging {
 		RowMapBuffer buffer = new RowMapBuffer(2, 250); // allow about 250 bytes of memory to be used
 
 		RowMap r;
-		buffer.add(new RowMap("insert", "foo", "bar", 1L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
-		buffer.add(new RowMap("insert", "foo", "bar", 2L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
-		buffer.add(new RowMap("insert", "foo", "bar", 3L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
+		buffer.add(new RowMap("insert", "foo", "bar", 1000L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
+		buffer.add(new RowMap("insert", "foo", "bar", 2000L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
+		buffer.add(new RowMap("insert", "foo", "bar", 3000L, new ArrayList<String>(), new Position(new BinlogPosition(3, "mysql.1"), 0L)));
 
 		assertThat(buffer.size(), is(3L));
 		assertThat(buffer.inMemorySize(), is(2L));
