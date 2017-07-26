a2c3cd9c7558ec37ab2fd2f815cebd8afbd4c087
Ben Osheroff <ben@zendesk.com>
2017-07-26 10:36:34 -0700

    setup proper defaults for the bootstrap utility
    addresses #705, had to refactor some to use common code.

--- src/main/java/com/zendesk/maxwell/MaxwellConfig.java
+++ src/main/java/com/zendesk/maxwell/MaxwellConfig.java
@@ -232,54 +232,6 @@ public class MaxwellConfig extends AbstractConfig {
 	}
 
 
-	private String fetchOption(String name, OptionSet options, Properties properties, String defaultVal) {
-		if ( options != null && options.has(name) )
-			return (String) options.valueOf(name);
-		else if ( (properties != null) && properties.containsKey(name) )
-			return properties.getProperty(name);
-		else
-			return defaultVal;
-	}
-
-
-	private boolean fetchBooleanOption(String name, OptionSet options, Properties properties, boolean defaultVal) {
-		if ( options != null && options.has(name) ) {
-			if ( !options.hasArgument(name) )
-				return true;
-			else
-				return Boolean.valueOf((String) options.valueOf(name));
-		} else if ( (properties != null) && properties.containsKey(name) )
-			return Boolean.valueOf(properties.getProperty(name));
-		else
-			return defaultVal;
-	}
-
-	private Long fetchLongOption(String name, OptionSet options, Properties properties, Long defaultVal) {
-		String strOption = fetchOption(name, options, properties, null);
-		if ( strOption == null )
-			return defaultVal;
-		else {
-			try {
-				return Long.valueOf(strOption);
-			} catch ( NumberFormatException e ) {
-				usageForOptions("Invalid value for " + name + ", integer required", "--" + name);
-			}
-			return null; // unreached
-		}
-	}
-
-
-	private MaxwellMysqlConfig parseMysqlConfig(String prefix, OptionSet options, Properties properties) {
-		MaxwellMysqlConfig config = new MaxwellMysqlConfig();
-		config.host     = fetchOption(prefix + "host", options, properties, null);
-		config.password = fetchOption(prefix + "password", options, properties, null);
-		config.user     = fetchOption(prefix + "user", options, properties, null);
-		config.port     = Integer.valueOf(fetchOption(prefix + "port", options, properties, "3306"));
-		config.setJDBCOptions(
-		    fetchOption(prefix + "jdbc_options", options, properties, null));
-		return config;
-	}
-
 	private void parse(String [] argv) {
 		OptionSet options = buildOptionParser().parse(argv);
 
