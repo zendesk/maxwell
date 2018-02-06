72196d5d36fc04f77e722b27a14c199261a3b610
Jens Geyti <jens.geyti@performancehorizon.com>
2018-02-06 15:52:28 +0000

    Add missing import

--- src/main/java/com/zendesk/maxwell/row/RowMap.java
+++ src/main/java/com/zendesk/maxwell/row/RowMap.java
@@ -15,6 +15,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.regex.Pattern;
