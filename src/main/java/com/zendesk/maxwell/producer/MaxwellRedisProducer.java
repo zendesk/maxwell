343ed974ba7158593b8effbd19884280a489c3bc
Ben Osheroff <ben@gimbo.net>
2018-11-21 13:27:45 -0600

    add some reconnect logic to redis
    addresses #1161

--- src/main/java/com/zendesk/maxwell/producer/MaxwellRedisProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/MaxwellRedisProducer.java
@@ -6,6 +6,7 @@ import com.zendesk.maxwell.util.StoppableTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class MaxwellRedisProducer extends AbstractProducer implements StoppableTask {
 	private static final Logger logger = LoggerFactory.getLogger(MaxwellRedisProducer.class);
@@ -33,48 +34,60 @@ public class MaxwellRedisProducer extends AbstractProducer implements StoppableT
 
 	@Override
 	public void push(RowMap r) throws Exception {
+		int cxErrors = 0;
 		if ( !r.shouldOutput(outputConfig) ) {
 			context.setPosition(r.getNextPosition());
 			return;
 		}
 
 		String msg = r.toJSON(outputConfig);
-		try {
-			switch (redistype){
-				case "lpush":
-					jedis.lpush(this.listkey, msg);
-					break;
-				case "pubsub":
-				default:
-					jedis.publish(this.channel, msg);
-					break;
-			}
-			this.succeededMessageCount.inc();
-			this.succeededMessageMeter.mark();
-		} catch (Exception e) {
-			this.failedMessageCount.inc();
-			this.failedMessageMeter.mark();
-			logger.error("Exception during put", e);
+		while ( true ) {
+			try {
+				switch (redistype) {
+					case "lpush":
+						jedis.lpush(this.listkey, msg);
+						break;
+					case "pubsub":
+					default:
+						jedis.publish(this.channel, msg);
+						break;
+				}
+				this.succeededMessageCount.inc();
+				this.succeededMessageMeter.mark();
+			} catch (Exception e) {
+				if (e instanceof JedisConnectionException && cxErrors++ < 2) {
+					logger.warn("lost connection to server, trying to reconnect...", e);
+					jedis.disconnect();
+					jedis.connect();
+					continue;
+				} else {
+					this.failedMessageCount.inc();
+					this.failedMessageMeter.mark();
+					logger.error("Exception during put", e);
 
-			if (!context.getConfig().ignoreProducerError) {
-				throw new RuntimeException(e);
+					if (!context.getConfig().ignoreProducerError) {
+						throw new RuntimeException(e);
+					}
+				}
 			}
-		}
 
-		if ( r.isTXCommit() ) {
-			context.setPosition(r.getNextPosition());
-		}
-
-		if ( logger.isDebugEnabled()) {
-			switch (redistype){
-				case "lpush":
-					logger.debug("->  queue:" + listkey + ", msg:" + msg);
-					break;
-				case "pubsub":
-				default:
-					logger.debug("->  channel:" + channel + ", msg:" + msg);
-					break;
+			if (r.isTXCommit()) {
+				context.setPosition(r.getNextPosition());
 			}
+
+			if (logger.isDebugEnabled()) {
+				switch (redistype) {
+					case "lpush":
+						logger.debug("->  queue:" + listkey + ", msg:" + msg);
+						break;
+					case "pubsub":
+					default:
+						logger.debug("->  channel:" + channel + ", msg:" + msg);
+						break;
+				}
+			}
+
+			break;
 		}
 	}
 
