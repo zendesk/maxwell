01a370e3a033f969b5c1f7333798d50948f3d1fb
Ben Osheroff <ben@gimbo.net>
2019-01-09 20:37:00 -0800

    update fancy GTID test
    jesus it's fancy.

--- src/test/java/com/zendesk/maxwell/replication/BinlogConnectorReplicatorTest.java
+++ src/test/java/com/zendesk/maxwell/replication/BinlogConnectorReplicatorTest.java
@@ -4,6 +4,7 @@ import com.github.shyiko.mysql.binlog.event.EventType;
 import com.github.shyiko.mysql.binlog.event.TableMapEventData;
 import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
 import com.github.shyiko.mysql.binlog.event.deserialization.EventDeserializer;
+import com.github.shyiko.mysql.binlog.event.deserialization.TableMapEventDataDeserializer;
 import com.github.shyiko.mysql.binlog.event.deserialization.WriteRowsEventDataDeserializer;
 import com.github.shyiko.mysql.binlog.io.ByteArrayInputStream;
 import com.zendesk.maxwell.MaxwellContext;
@@ -17,12 +18,35 @@ import com.zendesk.maxwell.row.RowMap;
 import com.zendesk.maxwell.schema.MysqlSchemaStore;
 import org.junit.Test;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class BinlogConnectorReplicatorTest extends TestWithNameLogging {
 
+	private class TestTableMapEventDeserializer extends TableMapEventDataDeserializer {
+		public HashMap<Long, TableMapEventData> map;
+
+		public TestTableMapEventDeserializer() {
+			this.map = new HashMap<>();
+		}
+
+		@Override
+		public TableMapEventData deserialize(ByteArrayInputStream inputStream) throws IOException {
+			TableMapEventData data = super.deserialize(inputStream);
+			map.put(data.getTableId(), data);
+			return data;
+		}
+	}
+
+	public final static int THROWME = 333;
 	private class DisconnectingDeserializer	extends WriteRowsEventDataDeserializer {
+		private boolean thrown = false;
 		public DisconnectingDeserializer(Map<Long, TableMapEventData> tableMapEventByTableId) {
 			super(tableMapEventByTableId);
 		}
@@ -30,8 +54,16 @@ public class BinlogConnectorReplicatorTest extends TestWithNameLogging {
 		@Override
 		public WriteRowsEventData deserialize(ByteArrayInputStream inputStream) throws IOException {
 			WriteRowsEventData d = super.deserialize(inputStream);
-			System.out.println(d.getRows());
-			//if ( d.getRows().get(0).toString().equals("hello"))
+			List<Serializable[]> list = d.getRows();
+			if ( !thrown && list.get(0)[0] instanceof Integer ) {
+				Integer i = (Integer) list.get(0)[0];
+				if ( i == THROWME ) {
+					thrown = true;
+					inputStream.close();
+					throw new EOFException();
+				}
+			}
+
 			return d;
 		}
 	}
@@ -39,10 +71,12 @@ public class BinlogConnectorReplicatorTest extends TestWithNameLogging {
 	@Test
 	public void testGTIDReconnects() throws Exception {
 		MysqlIsolatedServer server = MaxwellTestSupport.setupServer("--gtid_mode=ON --enforce-gtid-consistency=true");
-		MaxwellTestSupport.setupSchema(server);
+		MaxwellTestSupport.setupSchema(server, false);
 
 		server.execute("create table test.t ( i int )");
-		server.execute("insert into test.t set i = 1");
+		server.execute("create table test.u ( i int )");
+		// prime GTID set
+		server.execute("insert into test.t set i = 111");
 
 		Position position = Position.capture(server.getConnection(), true);
 		MaxwellContext context = MaxwellTestSupport.buildContext(server.getPort(), position, null);
@@ -73,13 +107,27 @@ public class BinlogConnectorReplicatorTest extends TestWithNameLogging {
 			EventDeserializer.CompatibilityMode.CHAR_AND_BINARY_AS_BYTE_ARRAY,
 			EventDeserializer.CompatibilityMode.INVALID_DATE_AND_TIME_AS_MIN_VALUE
 		);
-		eventDeserializer.setEventDataDeserializer(EventType.EXT_WRITE_ROWS, new DisconnectingDeserialzier());
 
-		replicator.client.setEventDeserializer();
+		TestTableMapEventDeserializer tmd = new TestTableMapEventDeserializer();
+		eventDeserializer.setEventDataDeserializer(EventType.TABLE_MAP, tmd);
+		DisconnectingDeserializer dd = (DisconnectingDeserializer) new DisconnectingDeserializer(tmd.map).setMayContainExtraInformation(true);
+		eventDeserializer.setEventDataDeserializer(EventType.EXT_WRITE_ROWS, dd);
+		replicator.client.setEventDeserializer(eventDeserializer);
 
 		replicator.startReplicator();
+		// prime up maxwell, let it capture schema and such.
+		server.execute("insert into test.t set i = 111");
+		while ( replicator.getRow() != null ) { }
 
-		server.execute("insert into test.t set i = 2");
-		RowMap r = replicator.getRow();
+
+		server.getConnection().setAutoCommit(false);
+		server.execute("BEGIN");
+		server.execute("insert into test.t set i = 222");
+		server.execute("insert into test.u set i = " + THROWME);
+		server.execute("COMMIT");
+
+		assertEquals(222L, replicator.getRow().getData().get("i"));
+		assertEquals(333L, replicator.getRow().getData().get("i"));
+		assertEquals(null, replicator.getRow());
 	}
 }
