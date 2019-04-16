1977f617c20bce6b2973696958f209420b84c230
Ben Osheroff <ben@gimbo.net>
2019-04-15 22:18:11 -0700

    add .partition_string to filter.js example

--- src/example/filter.js
+++ src/example/filter.js
@@ -8,6 +8,7 @@
  *
  * .suppress() -> call this to remove row from output
  * .kafka_topic = "topic" -> override the default kafka topic for this row
+ * .partition_string = "string" -> override kafka/kinesis partition-by string
  * .data -> hash containing row data.  May be modified.
  * .old_data -> hash containing old data
  * .extra_attributes -> if you set values in this hash, they will be output at the top level of the final JSON
