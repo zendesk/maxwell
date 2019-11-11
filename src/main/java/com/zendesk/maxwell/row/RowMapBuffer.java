526933211a6fab43ddd079939ed7db6153a8880d
sudipta2016 <sudipta.samanta@capillarytech.com>
2019-11-11 23:23:56 +0530

    reset buffer memory

--- src/main/java/com/zendesk/maxwell/row/RowMapBuffer.java
+++ src/main/java/com/zendesk/maxwell/row/RowMapBuffer.java
@@ -16,7 +16,7 @@ public class RowMapBuffer extends ListWithDiskBuffer<RowMap> {
 
 	public RowMapBuffer(long maxInMemoryElements) {
 		super(maxInMemoryElements);
-		this.maxMemory = (long) (Runtime.getRuntime().maxMemory() * 0.5);
+		this.maxMemory = (long) (Runtime.getRuntime().maxMemory() * 0.35);
 	}
 
 	public RowMapBuffer(long maxInMemoryElements, long maxMemory) {
