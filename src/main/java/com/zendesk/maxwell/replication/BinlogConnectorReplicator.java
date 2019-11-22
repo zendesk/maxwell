c38f2e6cf065e755dce8745cdfefe9f092f70df8
sudipta2016 <sudipta.samanta@capillarytech.com>
2019-11-22 18:15:45 +0530

    updated pom version

--- src/main/java/com/zendesk/maxwell/replication/BinlogConnectorReplicator.java
+++ src/main/java/com/zendesk/maxwell/replication/BinlogConnectorReplicator.java
@@ -184,7 +184,6 @@ public class BinlogConnectorReplicator extends AbstractReplicator implements Rep
 				case EXT_UPDATE_ROWS:
 				case EXT_DELETE_ROWS:
 					Table table = tableCache.getTable(event.getTableID());
-
 					if ( table != null && shouldOutputEvent(table.getDatabase(), table.getName(), filter, table.getColumnNames()) ) {
 						for ( RowMap r : event.jsonMaps(table, getLastHeartbeatRead(), currentQuery) )
 							if (shouldOutputRowMap(table.getDatabase(), table.getName(), r, filter)) {
