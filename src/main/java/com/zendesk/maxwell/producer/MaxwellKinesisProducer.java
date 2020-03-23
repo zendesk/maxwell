09822285e1c69465bbebb7c769b3ec84f3c54db1
Rotem Fogel <rotem@seekingalpha.com>
2020-03-23 15:58:00 +0200

    fix remove of pk

--- src/main/java/com/zendesk/maxwell/producer/MaxwellKinesisProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/MaxwellKinesisProducer.java
@@ -3,7 +3,11 @@ package com.zendesk.maxwell.producer;
 import com.amazonaws.services.kinesis.producer.*;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Meter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -14,14 +18,16 @@ import com.zendesk.maxwell.row.RowMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 
@@ -31,12 +37,12 @@ class KinesisCallback implements FutureCallback<UserRecordResult> {
 	private final AbstractAsyncProducer.CallbackCompleter cc;
 	private final Position position;
 	private final String json;
-	private MaxwellContext context;
+	private final MaxwellContext context;
 	private final String key;
-	private Counter succeededMessageCount;
-	private Counter failedMessageCount;
-	private Meter succeededMessageMeter;
-	private Meter failedMessageMeter;
+	private final Counter succeededMessageCount;
+	private final Counter failedMessageCount;
+	private final Meter succeededMessageMeter;
+	private final Meter failedMessageMeter;
 
 	public KinesisCallback(AbstractAsyncProducer.CallbackCompleter cc, Position position, String key, String json,
 						   Counter producedMessageCount, Counter failedMessageCount, Meter producedMessageMeter,
@@ -95,6 +101,8 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 	private final KinesisProducer kinesisProducer;
 	private final String kinesisStream;
 	private final boolean kinesisLinebreak;
+	private final ObjectMapper jsonParser;
+	private final Cache<String, Set<String>> cache;
 
 	public MaxwellKinesisProducer(MaxwellContext context, String kinesisStream) {
 		super(context);
@@ -106,6 +114,13 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 		this.partitioner = new MaxwellKinesisPartitioner(partitionKey, partitionColumns, partitionFallback, kinesisMd5Keys);
 		this.kinesisStream = kinesisStream;
 		this.kinesisLinebreak = context.getConfig().kinesisLinebreak;
+		this.jsonParser = new ObjectMapper();
+
+
+		this.cache = CacheBuilder.newBuilder()
+				.maximumSize(500)
+				.expireAfterWrite(1, TimeUnit.HOURS)
+				.build();
 
 		Path path = Paths.get("kinesis-producer-library.properties");
 		if (Files.exists(path) && Files.isRegularFile(path)) {
@@ -116,6 +131,8 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 		}
 	}
 
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public void sendAsync(RowMap r, AbstractAsyncProducer.CallbackCompleter cc) throws Exception {
 		String key = this.partitioner.getKinesisKey(r);
@@ -126,15 +143,69 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 		int vsize = value.length();
 		final long maxValueSize = context.getConfig().producerMaxValueSize;
 
-		// get rid of largest value recursively to accommodate max_data_length
-		if (vsize > maxValueSize) {
-			logger.warn("{}.{} with key {} has size of {}. Removing largest values", r.getDatabase(), r.getTable(), key, vsize);
-			LinkedHashMap<String, Object> data = r.getData();
-			while (vsize > maxValueSize) {
-				Optional<Map.Entry<String, Object>> maxEntry = data.entrySet().stream().max(Comparator.comparing(e -> String.valueOf(e.getValue()).length()));
+		final String db = r.getDatabase();
+		final String table = r.getTable();
+
+		// extract primary key/s from RowMap
+		Function<String, Set<String>> pkFromJson = (json) -> {
+			try {
+				String jsonData = r.pkToJson(RowMap.KeyFormat.HASH);
+				Map<String, Object> map = jsonParser.readValue(jsonData, Map.class);
+				return map.keySet().stream().filter(k -> k.startsWith("pk")).map(k -> {
+					String[] parts = k.split("\\.");
+					return parts[1];
+				}).collect(Collectors.toSet());
+			} catch (IOException e) {
+				logger.error("error identifying table {}.{} primary keys", db, table, e);
+				return Sets.newHashSet("id");
+			}
+		};
+
+		final Set<String> keys = cache.get(table, () -> pkFromJson.apply(r.pkToJson(RowMap.KeyFormat.ARRAY)));
+		LinkedHashMap<String, Object> reserve = new LinkedHashMap<>();
+
+		Function<LinkedHashMap<String, Object>, String> largestValueFn = (dataHolder) -> {
+			try {
+				String val = r.toJSON(outputConfig);
+				Optional<Map.Entry<String, Object>> maxEntry = dataHolder.entrySet().stream().max(Comparator.comparing(e -> String.valueOf(e.getValue()).length()));
 				if (maxEntry.isPresent()) {
-					data.remove(maxEntry.get().getKey());
+					Map.Entry<String, Object> entry = maxEntry.get();
+					final String entryKey = entry.getKey();
+					// if one of the table's primary key columns is to be removed, reserve it
+					if (keys.contains(entryKey)) {
+						// save the id column
+						reserve.put(entryKey, entry.getValue());
+					}
+					dataHolder.remove(entryKey);
+					val = r.toJSON(outputConfig);
+					if (kinesisLinebreak) {
+						val = val + '\n';
+					}
+				}
+				return val;
+			} catch (Exception e) {
+				logger.error("error extracting the largest value", e);
+				return null;
+			}
+		};
+
+		// get rid of the largest value recursively to accommodate the max_data_length
+		// first remove oldData, then newData
+		if (vsize > maxValueSize) {
+			logger.warn("{}.{} with key {} has size of {}. Removing the largest values", db, table, key, vsize);
+			LinkedHashMap<String, Object> data = r.getData();
+			LinkedHashMap<String, Object> oldData = r.getOldData();
+			while (vsize > maxValueSize) {
+				String tmpValue = largestValueFn.apply(oldData);
+				if (tmpValue != null) {
 					value = r.toJSON(outputConfig);
+					vsize = value.length();
+					if (vsize > maxValueSize) {
+						tmpValue = largestValueFn.apply(data);
+						if (tmpValue != null) {
+							value = r.toJSON(outputConfig);
+						}
+					}
 					if (this.kinesisLinebreak) {
 						value = value + '\n';
 					}
@@ -142,10 +213,17 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 				} else
 					break;
 			}
-			logger.info("{}.{} with key {} reduced to size {}", r.getDatabase(), r.getTable(), key, vsize);
+			logger.info("{}.{} with key {} reduced to size {}", db, table, key, vsize);
+			if (!reserve.isEmpty()) {
+				data.putAll(reserve);
+				value = r.toJSON(outputConfig);
+				if (this.kinesisLinebreak) {
+					value = value + '\n';
+				}
+			}
 		}
 
-		ByteBuffer encodedValue = ByteBuffer.wrap(value.getBytes("UTF-8"));
+		ByteBuffer encodedValue = ByteBuffer.wrap(value.getBytes(StandardCharsets.UTF_8));
 
 		// release the reference to ease memory pressure
 		if (!KinesisCallback.logger.isDebugEnabled()) {
@@ -160,7 +238,7 @@ public class MaxwellKinesisProducer extends AbstractAsyncProducer {
 			Futures.addCallback(future, callback, directExecutor());
 		} catch (IllegalArgumentException t) {
 			callback.onFailure(t);
-			logger.error("Database:" + r.getDatabase() + ", Table:" + r.getTable() + ", PK:" + r.getRowIdentity().toConcatString() + ", Size:" + vsize);
+			logger.error("Database:" + db + ", Table:" + table + ", PK:" + r.getRowIdentity().toConcatString() + ", Size:" + vsize);
 		}
 	}
 
