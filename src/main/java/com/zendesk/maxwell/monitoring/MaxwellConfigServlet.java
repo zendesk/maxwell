5d2254f1ee06e976ed56827a4ede6d5ee420a18d
Mark Crisp <mark.crisp@adp.com>
2021-03-29 12:59:11 -0400

    fix indentation

--- src/main/java/com/zendesk/maxwell/monitoring/MaxwellConfigServlet.java
+++ src/main/java/com/zendesk/maxwell/monitoring/MaxwellConfigServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zendesk.maxwell.MaxwellContext;
@@ -20,99 +19,101 @@ import java.io.PrintWriter;
 import java.util.stream.Collectors;
 
 /**
- * An HTTP servlet which allows for the live reconfiguration of maxwell.
- * If property provided for update is null, it will be ignored.
+ * An HTTP servlet which allows for the live reconfiguration of maxwell. If
+ * property provided for update is null, it will be ignored.
  */
 public class MaxwellConfigServlet extends HttpServlet {
-  private static final java.lang.String CONTENT_TYPE = "application/json";
-  private static final ObjectMapper mapper = new ObjectMapper();
-  static final Logger LOGGER = LoggerFactory.getLogger(MaxwellConfigServlet.class);
-  private final MaxwellContext context;
+	private static final java.lang.String CONTENT_TYPE = "application/json";
+	private static final ObjectMapper mapper = new ObjectMapper();
+	static final Logger LOGGER = LoggerFactory.getLogger(MaxwellConfigServlet.class);
+	private final MaxwellContext context;
 
+	public MaxwellConfigServlet(MaxwellContext context) {
+		super();
+		this.context = context;
+	}
 
-  public MaxwellConfigServlet(MaxwellContext context) {
-    super();
-    this.context = context;
-  }
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	private static class SerializedConfig {
+		public String filter;
+	}
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  private static class SerializedConfig {
-    public String filter;
-  }
+	private static class ErrorResponse {
+		public int code;
+		public String message;
 
-  private static class ErrorResponse {
-    public int code;
-    public String message;
+		public ErrorResponse(int code, String message) {
+			this.code = code;
+			this.message = message;
+		}
 
-    public ErrorResponse(int code, String message) {
-      this.code = code;
-      this.message = message;
-    }
+		public static String getErrorResponseString(int code, String message) throws JsonProcessingException {
+			ErrorResponse errResponse = new ErrorResponse(code, message);
+			return String.format("{\"error\":%s}", mapper.writeValueAsString(errResponse));
+		}
+	}
 
-    public static String getErrorResponseString(int code, String message) throws JsonProcessingException {
-      ErrorResponse errResponse = new ErrorResponse(code, message);
-      return String.format("{\"error\":%s}",  mapper.writeValueAsString(errResponse));
-    }
-  }
+	protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		SerializedConfig sconfig;
+		try {
+			String reqbody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+			sconfig = mapper.readValue(reqbody, SerializedConfig.class);
+		} catch (Exception e) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType(CONTENT_TYPE);
+			try (PrintWriter writer = resp.getWriter()) {
+				writer.println(ErrorResponse.getErrorResponseString(HttpServletResponse.SC_BAD_REQUEST,
+						String.format("error processing body: %s", e.getMessage())));
+			}
+			return;
+		}
 
-  protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    SerializedConfig sconfig;
-    try {
-      String reqbody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-      sconfig = mapper.readValue(reqbody, SerializedConfig.class);
-    } catch(Exception e) {
-      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      resp.setContentType(CONTENT_TYPE);
-      try (PrintWriter writer = resp.getWriter()) {
-        writer.println(ErrorResponse.getErrorResponseString(HttpServletResponse.SC_BAD_REQUEST, String.format("error processing body: %s", e.getMessage())));
-      }
-      return;
-    }
+		try {
+			applySerializedConfig(sconfig);
 
-    try {
-      applySerializedConfig(sconfig);
+			resp.setStatus(HttpServletResponse.SC_OK);
+			resp.setContentType(CONTENT_TYPE);
+			try (PrintWriter writer = resp.getWriter()) {
+				writer.println(getSerializedConfig());
+			}
+		} catch (InvalidFilterException e) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType(CONTENT_TYPE);
+			try (PrintWriter writer = resp.getWriter()) {
+				writer.println(ErrorResponse.getErrorResponseString(HttpServletResponse.SC_BAD_REQUEST,
+						String.format("invalid filter: %s", e.getMessage())));
+			}
+		}
+	}
 
-      resp.setStatus(HttpServletResponse.SC_OK);
-      resp.setContentType(CONTENT_TYPE);
-      try (PrintWriter writer = resp.getWriter()) {
-        writer.println(getSerializedConfig());
-      }
-    } catch (InvalidFilterException e) {
-      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      resp.setContentType(CONTENT_TYPE);
-      try (PrintWriter writer = resp.getWriter()) {
-        writer.println(ErrorResponse.getErrorResponseString(HttpServletResponse.SC_BAD_REQUEST, String.format("invalid filter: %s", e.getMessage())));
-      }
-    }
-  }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.setContentType(CONTENT_TYPE);
+		try (PrintWriter writer = resp.getWriter()) {
+			writer.println(getSerializedConfig());
+		}
+	}
 
-  @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setStatus(HttpServletResponse.SC_OK);
-    resp.setContentType(CONTENT_TYPE);
-      try (PrintWriter writer = resp.getWriter()) {
-        writer.println(getSerializedConfig());
-      }
-  }
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getMethod().equalsIgnoreCase("PATCH")) {
+			doPatch(request, response);
+		} else {
+			super.service(request, response);
+		}
+	}
 
-  @Override
-  public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    if (request.getMethod().equalsIgnoreCase("PATCH")) {
-      doPatch(request, response);
-    } else {
-      super.service(request, response);
-    }
-  }
+	private String getSerializedConfig() throws JsonProcessingException {
+		SerializedConfig sconfig = new SerializedConfig();
+		sconfig.filter = this.context.getFilter().toString();
+		return mapper.writeValueAsString(sconfig);
+	}
 
-  private String getSerializedConfig() throws JsonProcessingException {
-    SerializedConfig sconfig = new SerializedConfig();
-    sconfig.filter = this.context.getFilter().toString();
-    return mapper.writeValueAsString(sconfig);
-  }
-
-  private void applySerializedConfig(SerializedConfig sconfig) throws InvalidFilterException {
-    if (sconfig.filter != null) {
-      context.getFilter().set(sconfig.filter);
-      LOGGER.info("updated filter: " + sconfig.filter);
-    }
-  }
+	private void applySerializedConfig(SerializedConfig sconfig) throws InvalidFilterException {
+		if (sconfig.filter != null) {
+			context.getFilter().set(sconfig.filter);
+			LOGGER.info("updated filter: " + sconfig.filter);
+		}
+	}
 }
