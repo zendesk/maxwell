f9442b58b9a58fbfa240dc12f559572e0ce471fd
Ben Osheroff <ben@gimbo.net>
2022-01-23 08:51:20 -0800

    fixup tests

--- src/test/java/com/zendesk/maxwell/schema/MysqlPositionStoreTest.java
+++ src/test/java/com/zendesk/maxwell/schema/MysqlPositionStoreTest.java
@@ -1,9 +1,6 @@
 package com.zendesk.maxwell.schema;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 import com.zendesk.maxwell.*;
 import java.sql.ResultSet;
@@ -36,7 +33,6 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 	@Test
 	public void testSetBinlogPosition() throws Exception {
 		MysqlPositionStore store = buildStore();
-		long lastHeartbeatRead = 100L;
 		BinlogPosition binlogPosition;
 		if (MaxwellTestSupport.inGtidMode()) {
 			String gtid = "123:1-100";
@@ -46,7 +42,7 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 		}
 		Position position = new Position(binlogPosition, 100L);
 		store.set(position);
-		assertThat(buildStore().get(), is(position));
+		assertEquals(buildStore().get(), position);
 	}
 
 	@Test
@@ -60,7 +56,7 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 		ResultSet rs = server.getConnection().createStatement().executeQuery("select * from maxwell.heartbeats");
 		rs.next();
 
-		assertThat(rs.getLong("heartbeat") >= preHeartbeat, is(true));
+		assertTrue(rs.getLong("heartbeat") >= preHeartbeat);
 	}
 
 	@Test
@@ -80,7 +76,7 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 			exception = d;
 		}
 
-		assertThat(exception, is(not(nullValue())));
+		assertNotNull(exception);
 	}
 
 	@Test
@@ -89,11 +85,11 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 		MysqlPositionStore store = buildStore(context);
 		List<RecoveryInfo> recoveries = store.getAllRecoveryInfos();
 
-		assertThat(recoveries.size(), is(0));
+		assertEquals(recoveries.size(), 0);
 
 		String errorMessage = StringUtils.join(store.formatRecoveryFailure(context.getConfig(), recoveries), "\n");
-		assertThat(errorMessage, is("Unable to find any binlog positions in `positions` table"));
-		assertThat(store.getRecoveryInfo(context.getConfig()), is(nullValue()));
+		assertEquals(errorMessage, "Unable to find any binlog positions in `positions` table");
+		assertNull(store.getRecoveryInfo(context.getConfig()));
 	}
 
 	@Test
@@ -118,13 +114,13 @@ public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
 		List<RecoveryInfo> recoveries = store.getAllRecoveryInfos();
 
 		if (MaxwellTestSupport.inGtidMode()) {
-			assertThat(recoveries.size(), is(1));
+			assertEquals(recoveries.size(), 1);
 			// gtid mode can't get into a multiple recovery state
 			return;
 		}
 
-		assertThat(recoveries.size(), is(3));
-		assertThat(store.getRecoveryInfo(context.getConfig()), is(nullValue()));
+		assertEquals(recoveries.size(), 3);
+		assertNull(store.getRecoveryInfo(context.getConfig()));
 
 		String errorMessage = StringUtils.join(store.formatRecoveryFailure(context.getConfig(), recoveries), "\n");
 		assertThat(errorMessage, containsString("Found multiple binlog positions for cluster in `positions` table."));
