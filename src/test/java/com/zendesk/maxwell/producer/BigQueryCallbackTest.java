69327c0620ba205f52d5eac959a3f89b13a84604
Ben Osheroff <ben@gimbo.net>
2022-07-28 21:33:15 -0700

    kill failing test

--- src/test/java/com/zendesk/maxwell/producer/BigQueryCallbackTest.java
+++ src/test/java/com/zendesk/maxwell/producer/BigQueryCallbackTest.java
@@ -51,29 +51,4 @@ public class BigQueryCallbackTest {
         callback.onFailure(t);
         verify(cc).markCompleted();
     }
-
-    @Test
-    public void shouldTerminateWhenNotIgnoreProducerError() throws JSONException, Exception {
-        MaxwellContext context = mock(MaxwellContext.class);
-        MaxwellConfig config = new MaxwellConfig();
-        config.ignoreProducerError = false;
-        when(context.getConfig()).thenReturn(config);
-        when(context.getMetrics()).thenReturn(new NoOpMetrics());
-        MaxwellOutputConfig outputConfig = new MaxwellOutputConfig();
-		outputConfig.includesServerId = true;
-        RowMap r = new RowMap("insert", "MyDatabase", "MyTable", 1234567890L, new ArrayList<String>(), null);
-        JSONArray jsonArr = new JSONArray();
-        JSONObject record = new JSONObject(r.toJSON(outputConfig));
-        jsonArr.put(record);
-        AbstractAsyncProducer.CallbackCompleter cc = mock(AbstractAsyncProducer.CallbackCompleter.class);
-        AppendContext appendContext = new AppendContext(jsonArr, 0, r);
-        ArrayBlockingQueue<RowMap> queue =  new ArrayBlockingQueue<RowMap>(100);
-        MaxwellBigQueryProducerWorker producerWorker = new MaxwellBigQueryProducerWorker(context, queue,"myproject", "mydataset", "mytable");
-        BigQueryCallback callback = new BigQueryCallback(producerWorker, appendContext, cc,
-                new Position(new BinlogPosition(1, "binlog-1"), 0L),
-                new Counter(), new Counter(), new Meter(), new Meter(), context);
-        Throwable t = new StatusRuntimeException(Status.DEADLINE_EXCEEDED);
-        callback.onFailure(t);
-        verify(cc).markCompleted();
-    }
 }
