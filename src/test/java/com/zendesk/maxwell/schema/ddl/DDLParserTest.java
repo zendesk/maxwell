08a7bd5079eeae0f7a8f322dc12b90a02ceea2e0
Ben Osheroff <ben@gimbo.net>
2023-03-11 04:22:02 -0800

    add collation test

--- src/test/java/com/zendesk/maxwell/schema/ddl/DDLParserTest.java
+++ src/test/java/com/zendesk/maxwell/schema/ddl/DDLParserTest.java
@@ -265,7 +265,8 @@ public class DDLParserTest {
 			"ALTER TABLE t1 ADD COLUMN IF NOT EXISTS c1 TINYINT",
 			"ALTER TABLE tournaments ADD INDEX idx_team_name (('$.teams.name'))",
 			"ALTER TABLE tournaments ADD INDEX idx_team_name ((ABS(col)))",
-			"ALTER TABLE tournaments ADD INDEX idx_team_name ((col1 * 40) DESC)"
+			"ALTER TABLE tournaments ADD INDEX idx_team_name ((col1 * 40) DESC)",
+			"CREATE TABLE employees (data JSON, INDEX idx ((CAST(data->>'$.name' AS CHAR(30)) COLLATE utf8mb4_bin)))"
 		};
 
 		for ( String s : testSQL ) {
