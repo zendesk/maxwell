56a00d0bfd2e6727a231268c44b94b451a3980af
Aniruddha Navare <aniruddha.navare@zendesk.com>
2025-05-19 11:24:32 +0530

    GO-2018 - Modified Java code

--- src/main/java/com/zendesk/maxwell/filtering/Filter.java
+++ src/main/java/com/zendesk/maxwell/filtering/Filter.java
@@ -1,6 +1,6 @@
 package com.zendesk.maxwell.filtering;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
